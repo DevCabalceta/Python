d16438f2778d6dd90604e1cb804e4f07788e273c
DevCabalceta <cabalceta.gabriel.2001@gmail.com>
2025-09-20 12:08:02 -0600

    inicios

---
+++ CLASE-01/Ejercicio2.py
@@ -0,0 +1,13 @@
+# Pascal Case
+# NombrePersona = "Gabriel Cabalceta Quiros"
+# Camel Case
+# nombrePersona = "Gabriel Cabalceta Quiros"
+
+nombrePersona = "Gabriel"
+apellidoPersona1 = "Cabalceta"
+apellidoPersona2 = "Quiros"
+
+print(nombrePersona)
+print(apellidoPersona1)
+print(apellidoPersona2)
+print(nombrePersona + " " + apellidoPersona1 + " " + apellidoPersona2)
---
+++ CLASE-01/Ejercicio3.py
@@ -0,0 +1,16 @@
+
+valor1 = 5
+valor2 = 10
+
+print (valor1 + valor2)
+
+valor1 = 20
+print (valor1 + valor2)
+
+valor1 = "7"
+valor2 = str(8)
+print (valor1 + valor2)
+
+valor1 = int("7")
+valor2 = 8
+print (valor1 + valor2)
